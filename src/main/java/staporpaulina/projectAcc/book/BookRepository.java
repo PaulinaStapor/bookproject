74c174f622259deb81cd79e3c88601818c9f3120
Stapor <paulina.stapor@accenture.com>
2019-01-23 10:45:10 +0100

    book and author repository

---
+++ src/main/java/staporpaulina/projectAcc/book/BookRepository.java
@@ -0,0 +1,19 @@
+package staporpaulina.projectAcc.book;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BookRepository extends JpaRepository <Book, Long> {
+
+    @Query("SELECT b FROM Book b ORDER BY b.dateOfPublication")
+    List<Book> findAll();
+
+    @Query("SELECT b FROM Book b WHERE b.title LIKE %?1% ORDER BY b.dateOfPublication")
+    List<Book> findBookByTitle (String query);
+
+
+}
