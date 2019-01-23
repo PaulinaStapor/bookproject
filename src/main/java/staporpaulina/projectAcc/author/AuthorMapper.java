f7eba62e8b74a99d9d651d427d216d54c83d3946
Stapor <paulina.stapor@accenture.com>
2019-01-23 10:16:13 +0100

    book and author mappers

--- src/main/java/staporpaulina/projectAcc/author/AuthorMapper.java
+++ src/main/java/staporpaulina/projectAcc/author/AuthorMapper.java
@@ -1,4 +1,16 @@
 package staporpaulina.projectAcc.author;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
 public interface AuthorMapper {
+AuthorMapper INSTANCE= Mappers.getMapper(AuthorMapper.class);
+@Mapping(source = "firstName", target = "firstName")
+@Mapping(source = "lastName", target = "lastName")
+@Mapping(source = "dateOfBirth", target = "dateOfBirth")
+AuthorDTO authorToAuthorDTO(Author author);
+
+
 }
--- src/main/java/staporpaulina/projectAcc/book/BookMapper.java
+++ src/main/java/staporpaulina/projectAcc/book/BookMapper.java
@@ -1,4 +1,17 @@
 package staporpaulina.projectAcc.book;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
 public interface BookMapper {
+BookMapper INSTANCE= Mappers.getMapper(BookMapper.class);
+@Mapping(source = "title", target = "title")
+@Mapping(source = "pages", target = "pages")
+@Mapping(source = "description", target = "description")
+@Mapping(source = "dateOfPublication", target = "dateOfPublication")
+@Mapping(source = "author", target = "author")
+BookDTO bookToBookDTO (Book book);
+
 }
